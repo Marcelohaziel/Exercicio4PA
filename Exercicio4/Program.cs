57a6910bb4b0cd6847f71fe61e125a7f3804bfe1
Marcelo Haziel <bioondorxrx@gmail.com>
2018-09-26 19:03:49 -0300

    v1.0
    Exercicio 4
    Aula Programação e Algoritmos.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aluno maiores de idade");
+            Console.WriteLine();
+
+            int i = 1;
+            int n1 = 0;
+            int quantMaiores = 0;
+
+            while (i <= 8)
+            {
+                Console.Write("Digite a idade do  " + i + " aluno: ");
+                n1 = Convert.ToInt32(Console.ReadLine());
+
+                if (n1 >= 18)
+                {
+                    quantMaiores++;
+                }
+                i++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Quantidade de numeros pares: " + quantMaiores);
+
+            Console.ReadKey();
+        }
+    }
+}
